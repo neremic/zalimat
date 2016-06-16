83663667f5fc785d057f9a3575919c91189a2038
Norman Eremic <norman.eremic@zalando.de>
2016-06-16 16:27:32 +0200

    added tests for handling child components

--- test/ChartWithHeader.test.js
+++ test/ChartWithHeader.test.js
@@ -4,6 +4,9 @@ import React from 'react';
 
 import ChartWithHeader from '../src/components/ChartWithHeader.jsx'
 import Chart from '../src/components/Chart.jsx'
+import DisplayApp from '../src/components/DisplayApp.jsx'
+import DisplayAppAndVersion from '../src/components/DisplayAppAndVersion.jsx'
+
 import Label from 'react-bootstrap/lib/Label';
 import Button from 'react-bootstrap/lib/Button';
 
@@ -82,4 +85,57 @@ describe('<ChartWithHeader />', () => {
         expect(chartNode.prop("dataSet")).toBe(testProps.dataSet);
         expect(chartNode.prop("viewPortDateRange")).toBe(testProps.viewPortDateRange);
     });
+
+    it('renders a `chart` component including child components and sets some props to them', () => {
+        const testProps = {
+            title : 'my title',
+            viewPortDateRange : {
+                startDate : moment().subtract(1, "days").toDate(),
+                endDate : new Date(Date.now())
+            },
+            dataSet : createRandomData(),
+            onDelete : undefined,
+            applicationId : 'kio',
+            versionId : 'vId'
+        }
+
+        function createRandomData() {
+            const oneHour = 3600000;
+            let data = {label: '', values: []};
+            let startDate = new Date(2016, 5, 5);
+            let startMillis = startDate.getTime();
+            for (let i = 0; i < 60; i++) {
+                let multi = Math.floor(Math.random() * 5);
+                startMillis += oneHour * multi;
+                data.values.push({x: new Date(startMillis), y : Math.floor(Math.random() * 10)});
+            }
+            return data;
+        };
+
+
+        const wrapper = shallow(
+            <ChartWithHeader {...testProps}>
+                <DisplayApp someProp="propValue"/>
+                <DisplayAppAndVersion someProp="propValue2" someOtherProp="otherPropValue"/>
+            </ChartWithHeader>);
+        const chartNode = wrapper.find(Chart);
+        const childNode = wrapper.find(DisplayApp);
+        const otherChildNode = wrapper.find(DisplayAppAndVersion);
+
+        expect(chartNode.length).toBe(1);
+        expect(chartNode.prop("dataSet")).toBe(testProps.dataSet);
+        expect(chartNode.prop("viewPortDateRange")).toBe(testProps.viewPortDateRange);
+
+        expect(childNode.length).toBe(1);
+        expect(childNode.prop("someProp")).toBe('propValue');
+        expect(childNode.prop("applicationId")).toBe('kio');
+        expect(childNode.prop("versionId")).toBe('vId');
+
+        expect(otherChildNode.length).toBe(1);
+        expect(otherChildNode.prop("someProp")).toBe('propValue2');
+        expect(otherChildNode.prop("someOtherProp")).toBe('otherPropValue');
+        expect(otherChildNode.prop("applicationId")).toBe('kio');
+        expect(otherChildNode.prop("versionId")).toBe('vId');
+    });
+
 });
--- test/Charts.test.js
+++ test/Charts.test.js
@@ -1,8 +1,10 @@
 "use strict";
 
 import React from 'react';
+var createFragment = require('react-addons-create-fragment');
 
 import Charts from '../src/components/Charts.jsx'
+import DisplayApp from '../src/components/DisplayApp.jsx'
 import ChartWithHeader from '../src/components/ChartWithHeader.jsx'
 
 import moment from 'moment';
@@ -164,6 +166,7 @@ describe('<Charts />', () => {
         expect(chartNode.prop("versionId")).toBe('id1');
         expect(chartNode.prop("applicationId")).toBe(testProps.applicationId);
         expect(chartNode.prop("onDelete")).toEqual(testProps.onDelete);
+        expect(chartNode.prop("children")).toBe(undefined);
     });
 
     it('renders only matching versions data', () => {
@@ -210,6 +213,49 @@ describe('<Charts />', () => {
         expect(chartNode2.prop("versionId")).toBe('id2');
         expect(chartNode2.prop("applicationId")).toBe(testProps.applicationId);
         expect(chartNode2.prop("onDelete")).toEqual(testProps.onDelete);
+    });
+
+    it('renders the only matching version data and handles child components', () => {
+        const testProps = {
+            selectedVersions : [{value: 'id1', text: 'v1'}, {value: 'id2', text: 'v2'}],
+            onDelete : () => {},
+            viewPortDateRange : {
+                startDate : moment().subtract(1, "days").toDate(),
+                endDate : new Date(Date.now())
+            },
+            dataSets : (new Map()).set('id1', createRandomData()),
+            applicationId : 'kio'
+        }
+
+        function createRandomData() {
+            const oneHour = 3600000;
+            let data = {label: '', values: []};
+            let startDate = new Date(2016, 5, 5);
+            let startMillis = startDate.getTime();
+            for (let i = 0; i < 60; i++) {
+                let multi = Math.floor(Math.random() * 5);
+                startMillis += oneHour * multi;
+                data.values.push({x: new Date(startMillis), y : Math.floor(Math.random() * 10)});
+            }
+            return data;
+        };
+
+        const wrapper = shallow(<Charts {...testProps}><DisplayApp someProp="propValue"/></Charts>);
+        const chartNode = wrapper.childAt(0);
+        const childNode = chartNode.childAt(0);
+
+        expect(wrapper.children().length).toBe(1);
+        expect(chartNode.type()).toBe(ChartWithHeader);
+        expect(chartNode.prop("viewPortDateRange")).toEqual(testProps.viewPortDateRange);
+        expect(chartNode.prop("dataSet")).toEqual(testProps.dataSets.get('id1'));
+        expect(chartNode.prop("title")).toBe('v1');
+        expect(chartNode.prop("versionId")).toBe('id1');
+        expect(chartNode.prop("applicationId")).toBe(testProps.applicationId);
+        expect(chartNode.prop("onDelete")).toEqual(testProps.onDelete);
+
+        expect(chartNode.prop("children")).toNotBe(null);
+        expect(childNode.type()).toBe(DisplayApp);
+        expect(childNode.prop("someProp")).toBe('propValue');
     });
 
 });
