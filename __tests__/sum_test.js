d9722266f6c1a7e91d3ec8e213b3426f2a20e904
Nikolaus Piccolotto <prayerslayer@gmail.com>
2016-06-17 17:24:46 +0200

    Update sum_test.js

--- __tests__/sum_test.js
+++ __tests__/sum_test.js
@@ -1,5 +1,8 @@
 jest.unmock('../src/sum'); // unmock to use the actual implementation of sum
 
+/* Do we need to keep this file/folder? */
+
+
 describe('sum', () => {
     it('adds 1 + 2 to equal 3', () => {
         const sum = require('../src/sum');
